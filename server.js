5467f0c538dc9f56866158836ebd917337348f7d
David Walsh <davidliamwalsh@yahoo.co.uk>
2019-10-04 13:29:29 +0100

    commit after build

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare()
+app.prepare() 
 .then(() => {
   const server = express()
 
