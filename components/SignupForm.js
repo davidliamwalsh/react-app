69cde32704701d3c32a5010b77dd3f7d655782f8
David Walsh <davidliamwalsh@yahoo.co.uk>
2020-01-10 23:17:34 +0000

    article style added (#29)

--- components/SignupForm.js
+++ components/SignupForm.js
@@ -3,7 +3,7 @@ import { Component } from 'react'
 class SignupForm extends Component {
   render () {
     return <div className="c-header__signup">
-      <form action="https://facebook.us20.list-manage.com/subscribe/post?u=add0242531d662945b111ce32&amp;id=fed1dcab66" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate c-header__signup__form" target="_blank" noValidate>
+      <form action="https://heroku.us20.list-manage.com/subscribe/post?u=add0242531d662945b111ce32&amp;id=fed1dcab66" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate c-header__signup__form" target="_blank" noValidate>
         <div className="mc-field-group c-header__signup__form--field">
           <label htmlFor="mce-EMAIL" className="c-header__signup__form--field-label">
             Sign Up
