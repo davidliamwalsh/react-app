7c7d05e54eea8c361e67bc6175c0f0a9dbe3df33
David Walsh <davidliamwalsh@yahoo.co.uk>
2020-01-21 13:05:00 +0000

    scroll animation (#33)
    * articlecard keyframes (#27)
    
    * article style added (#29)
    
    * header with contact form postmark (#31)
    
    * ScrollAnimation added (#32)

--- components/Layout.js
+++ components/Layout.js
@@ -11,7 +11,8 @@ class Layout extends Component {
   render () {
     return <div>
       <MetaTags>
-        <link id="favicon" rel="icon" href="/static/DW-logo.jpg"></link>
+        <link id="favicon" rel="icon" href="/static/DW-logo.jpg" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
         <title>React Blooger</title>
         <meta id="meta-description" name="description" content="Hi, my name is David Walsh! I am from Leeds (UK), and I have a passion for Web Development! I am confident hand coding HTML5, CSS3 (Sass), and JavaScript. I have experience with jQuery, Ruby and RoR, TDD, OOP, using Version Control (GIT), Webpack, Bootstrap, and Photoshop. More recently I am working website security with JWT and Rack middleware." />
       </MetaTags>
--- server.js
+++ server.js
@@ -6,9 +6,14 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const postmark = require("postmark")
+const bodyParser = require('body-parser')
+
 app.prepare() 
 .then(() => {
   const server = express()
+  server.use(bodyParser.json())
+  server.use(bodyParser.urlencoded({ extended: true }))
 
   server.get('/', (req, res) => {
     return app.render(req, res, '/', req.query)
@@ -20,6 +25,29 @@ app.prepare()
 
   server.get('/contact', (req, res) => {
     return app.render(req, res, '/contact', req.query)
+  })
+
+  server.post('/contact-me', (req, res) => {
+    let client = new postmark.ServerClient(`${process.env.SERVER_TOKEN}`);
+    
+    client.sendEmail(
+      {
+        From: req.body.From,
+        To: req.body.To,
+        Subject: req.body.Name,
+        HtmlBody: req.body.HtmlBody,
+        ReplyTo: req.body.ReplyTo
+      }
+    ).then(response => {
+      console.log('Sending message')
+      console.log(response.To)
+      console.log(response.Message)
+      console.log(res.statusCode)
+      return res.sendStatus(200)
+    }).catch(error => {
+      console.log(error)
+      return res.sendStatus(500)
+    })
   })
 
   server.get('/article', (req, res) => {
