742f45c6f8f348236cd9df9b6f713164893c1c2a
David Walsh <davidliamwalsh@yahoo.co.uk>
2021-01-21 20:35:58 +0000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -18,7 +18,7 @@ class About extends Component {
           </div>
 
           <div className="c-about__intro">
-            I am confident hand coding HTML5, CSS3 (Sass), JavaScript, React, Ruby and RoR. I have experience with jQuery, GraphQL API, TDD, OOP, JWT, Version Control (GIT), Webpack, Bootstrap, and Photoshop. Previous experience with Liquid scripting when I was managing a Shopify site. More recently I am working with PHP, MySQL and WordPress to become a more rounded developer.<br />
+            I am confident hand coding HTML5, CSS3 (Sass), JavaScript, React, Ruby and RoR. I have experience with jQuery, GraphQL API, TDD, OOP, JWT, Version Control (GIT), Webpack, Bootstrap, and Photoshop. Experience with AWS, Heroku, and Docker. More recently I am working with Umbraco builds and creating customized websites..<br />
             Click<Link href="/static/davidDeveloper.docx"><a>here</a></Link>for an up to date copy of my CV, or you can view a brief overview below:
           </div>
 
